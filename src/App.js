765edc93d9eeeff40ecf963db4edcc7459c6f7ae
cklf <chandu537@gmail.com>
2022-10-28 10:47:33 -0700

    Fix FilterByTags dispach method.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useMemo } from 'react';
-// import './style.scss';
 import * as response from './data';
 import AppHeader from './component/header/header.component';
 import AppLayout from './component/layout/layout.component';
@@ -11,25 +10,6 @@ import {LOAD_DATA,FILTER_BY_TAGS } from './store/root.reducer'
 
 
 export default function App() {
-  /*
-  const [data, setData] = useState(null);
-  document.title = '< CIRQ />';
-  useMemo(() => {
-    setData(response.data);
-  }, [response]);
-
-  const filterByTags = (e)=>{
-    const filterQuestions = data.filter(question=>{
-      return question.type.includes(e.target.innerHTML)
-    })
-    setData(filterQuestions);
-  }
-
-  const dataOBJ = {
-    filterByTags,
-    data
-  }
-  */
 
   const dispatch = useDispatch();
   const loadIniData = ()=>{
--- src/store/root.reducer.js
+++ src/store/root.reducer.js
@@ -10,19 +10,13 @@ export const rootSlice = createSlice({
     initialState,
     reducers:{
         FILTER_BY_TAGS: (state, action)=>{
-            state.questions.data = state.questions.data.map(question=>question.type.includes(action.payload));
+            state.questions.data = state.questions.data.filter(question=>question.type.includes(action.payload));
         },
         LOAD_DATA:(state, action)=>{
             state.questions = action.payload;
         }
     }
 });
-
-
-const manu = (obj)=>{
-    obj[0].type = "abc";
-    console.log("obaSDASDj",obj);
-}
 
 export const {FILTER_BY_TAGS, LOAD_DATA} = rootSlice.actions;
 export default rootSlice.reducer;
